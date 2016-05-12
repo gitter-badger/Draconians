c25ba0fe321eac3c0678ce8c217d8a466d870685
EpicElric <epicelric.gaming@gmail.com>
2016-05-12 07:42:35 -0700

    Added Toolbar, Updated colors

--- app/src/main/java/com/epicelric/draconians/MainActivity.java
+++ app/src/main/java/com/epicelric/draconians/MainActivity.java
@@ -1,67 +1,48 @@
 package com.epicelric.draconians;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Toast;
-import android.util.Log;
-
-
-
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = MainActivity.class.getSimpleName();
+    //private static final String TAG = MainActivity.class.getSimpleName();
 
+    private Toolbar mToolbar;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
-        setTitle(getString(R.string.title_main));
-        Log.d(TAG, "onCreate()");
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
-        Log.d(TAG, "onCreateOptionsMenu() Inflate main_menu");
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-//              TODO: 5/11/2016 Add settings page
-//              Change to settings page
-                Toast.makeText(getApplicationContext(), "Settings Touched", Toast.LENGTH_SHORT).show();
-                Log.d(TAG, "onOptionsItemSelected() Settings pressed and toast sent");
-                return true;
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-
-            case R.id.action_members:
-                /*Change to members page*/
-                setContentView(R.layout.members);
-                Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-                setSupportActionBar(myToolbar);
-                setTitle(getString(R.string.title_members));
-                Log.d(TAG, "onOptionsItemSelected() Members pressed and change layout to members");
-                return true;
-
-
-            default:
-//                If we got here, the user's action was not recognized.
-//                Invoke the superclass to handle it.
-                return super.onOptionsItemSelected(item);
-
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
+
+        return super.onOptionsItemSelected(item);
     }
-
-
 }
